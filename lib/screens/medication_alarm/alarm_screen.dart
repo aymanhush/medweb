6b2637e5a1f92b2891c937a1b578295503aa31e0
ayman madwar <aimanmadwar858@gmail.com>
2023-06-24 00:33:02 +0300

    Delete Alarm

--- lib/screens/medication_alarm/alarm_screen.dart
+++ lib/screens/medication_alarm/alarm_screen.dart
@@ -68,9 +68,90 @@ class _Alarm_ScreenState extends State<Alarm_Screen> {
     );
   }
   _showBottumSheet(BuildContext context, Task task){
-
+    Get.bottomSheet(
+      Container(
+        padding: const EdgeInsets.only(top: 4),
+        height: task.isCompleted==1?
+        MediaQuery.of(context).size.height*0.24:
+        MediaQuery.of(context).size.height*0.32,
+        color: Get.isDarkMode?darkGreyClr:Colors.white,
+        child: Column(
+          children: [
+            Container(
+              height: 6,
+              width: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Get.isDarkMode?Colors.grey[600]:Colors.grey[300]
+              ),
+            ),
+            Spacer(),
+            task.isCompleted==1
+            ?Container()
+            : 
+            _bottomSheetButton(
+              label: "Delete Task", 
+              onTap: (){
+                _taskController.delete(task);
+                _taskController.getTasks();
+                Get.back();
+              }, 
+              clr: Colors.red[300]!,
+              context: context
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            _bottomSheetButton(
+              label: "Close", 
+              onTap: (){
+                Get.back();
+              }, 
+              clr: Colors.red[300]!,
+              isClose: true,
+              context: context
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ]),
+      ),
+    );
   }
-  
+
+  _bottomSheetButton({
+    required String label,
+    required Function()? onTap,
+    required Color clr,
+    bool isClose=false,
+    required BuildContext context,
+}){
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        height: 55,
+        width: MediaQuery.of(context).size.width*0.9,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 2,
+            color: isClose==true?Get.isDarkMode?Colors.grey[600]!:Colors.grey[300]!:clr
+            
+          ),
+          borderRadius: BorderRadius.circular(20),
+          color:isClose==true?Colors.transparent:clr,
+        ),
+
+        child: Center(
+          child: Text(
+            label,
+            style: isClose?titleStyle:titleStyle.copyWith(color: Colors.white),
+          ),
+        ),
+
+      ),
+    );
+  }
   _addDateBar(){
     return Container(
             margin: const EdgeInsets.only(top: 20, left: 20),
